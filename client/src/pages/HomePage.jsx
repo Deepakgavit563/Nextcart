3fbb4b05314031508943cf867198edb0e12392a3
Deepak Gavit <deepakgavit563@gmail.com>
2026-05-29 21:20:31 +0530

    feat: setup professional React frontend architecture

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        NextCart Ecommerce
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
