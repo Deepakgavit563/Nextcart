f0d56d4edbb28c00d589e50bcc14837d1de63897
Deepak Gavit <deepakgavit563@gmail.com>
2026-07-18 14:32:40 +0530

    feat(frontend): initialize React with Vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import { Provider } from "react-redux";
-
-import App from "./App";
-import { store } from "./store/store";
-
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
