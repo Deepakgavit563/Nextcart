80ab59e7997df86105950a12354456330296e6bf
Deepak Gavit <deepakgavit563@gmail.com>
2026-07-18 16:46:35 +0530

    chore(frontend): setup application structure

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      NextCart Frontend 🚀
-    </div>
-  );
-}
-
-export default App;
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,9 @@
+const AppRoutes = () => {
+  return (
+    <div>
+      Routes Coming Soon
+    </div>
+  );
+};
+
+export default AppRoutes;
