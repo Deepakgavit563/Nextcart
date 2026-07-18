e9d50abcde3f6946caad5e482295ec50b3a46983
Deepak Gavit <deepakgavit563@gmail.com>
2026-07-18 16:56:49 +0530

    feat(frontend): add feature based routing

---
+++ client/src/features/home/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>
+        NextCart Home 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,9 +1,53 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+import HomePage from "../features/home/pages/HomePage";
+import ProductsPage from "../features/products/pages/ProductsPage";
+import LoginPage from "../features/auth/pages/LoginPage";
+import RegisterPage from "../features/auth/pages/RegisterPage";
+import NotFoundPage from "../features/home/pages/NotFoundPage";
+
+
 const AppRoutes = () => {
   return (
-    <div>
-      Routes Coming Soon
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="/products"
+          element={<ProductsPage />}
+        />
+
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 };
+
 
 export default AppRoutes;
